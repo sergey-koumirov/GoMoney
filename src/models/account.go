694c7b8a63388908ec2b49db7fd42ade46b108e9
sergey-koumirov <sergey.koumirov@gmail.com>
2015-04-09 09:13:12 +0400

    currency id

--- src/models/account.go
+++ src/models/account.go
@@ -3,5 +3,11 @@ package models
 type Account struct {
     ID int64    `form:"ID"`
     Name string `form:"Name"`
+	CurrencyID string `form:"CurrencyID"`
+}
+
+type AccountForm struct {
+	T Account
+	CurrencyList []Currency
 }
 

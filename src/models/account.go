d3366851b35781f58e9c039fff51815c77cca9a6
sergey-koumirov <sergey.koumirov@gmail.com>
2015-04-09 21:15:57 +0400

    account types

--- src/models/account.go
+++ src/models/account.go
@@ -3,6 +3,7 @@ package models
 type Account struct {
     ID int64    `form:"ID"`
     Name string `form:"Name"`
+	Type string `form:"Type"`
 
 	Currency Currency
 	CurrencyID int64 `form:"CurrencyID"`
